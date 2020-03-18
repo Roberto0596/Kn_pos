b9ad18a7db9aac5cd5368c03e47cecfbfd7ccc15
roberto cordero <mago_ro_96@hotmail.com>
2020-03-17 22:23:31 -0700

    cambios

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,5 +1,5 @@
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2020 <a href="http://adminlte.io">DevCreative</a>.</strong>
+    <strong>Copyright &copy; 2020 <a href="http://adminlte.io">BlazarSoft</a>.</strong>
     All rights reserved.
   </footer>
