253116711cfc6d7d48c8b681cb2d0d315e6631d1
Wilber Lopez <wilber.lm12@gmail.com>
2020-03-20 15:33:16 -0700

    Footer

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,5 +1,5 @@
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2020 <a href="http://adminlte.io">BlazarSoft</a>.</strong>
+    <strong>Copyright &copy; 2020 <a href="https://blazar-soft.com">BlazarSoft</a>.</strong>
     All rights reserved.
   </footer>
